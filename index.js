c2f8a0908da32ac6ff183d9e0594e2b0b85e3d09
pcherian10 <pcherian10@gmail.com>
2019-02-16 15:11:11 -0500

    created state class

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let simpleLevelPlan = `
 ......##############..
 ......................`;
 
-class Level
+class Level {
     constructor(plan) {
         let rows = plan.trim().split("\n").map(l => [...l]);
         this.height = rows.length;
@@ -18,14 +18,28 @@ class Level
 
         this.rows = rows.map((row, y) => {
             return row.map((ch,x) => {
-                let type = levelChars[ch]
+                let type = levelChars[ch];
                 if (typeof type == "string") return type;
                 this.startActors.push(
                     type.create(new Vec(x, y), ch));
                     return "empty";
-                )
             })
         })
     }
+}
 
-class State
+class State {
+    constructor(level, actors, status) {
+        this.level = level;
+        this.actors = actors;
+        this.status = status;
+    }
+
+    static start(level) {
+        return new State(level, level.startActors, "playing");
+    }
+
+    get player() {
+        return this.actors.find(a => a.type == "player");
+    }
+}
